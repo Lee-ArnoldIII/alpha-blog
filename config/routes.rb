dd2fbfe7672fbfc349fc5443b971e999d0e7daac
Lee Arnold <lee.arnoldiii@gmail.com>
2021-05-11 20:37:23 -0500

    adding initial logic for user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :articles #control of what is exposed = only: [:show, :index, :new, :create, :edit, :update]
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
